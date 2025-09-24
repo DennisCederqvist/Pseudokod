388dbca06d3e8df68ef71c29e195534460b45c7a
DennisCederqvist <denniz.cederquist@gmail.com>
2025-09-24 12:04:48 +0200

    added another file with some psuedo code

---
+++ psuedo2.js
@@ -0,0 +1,27 @@
+
+
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+// Create a function that takes an integer as an argument and returns "Even" 
+// for even numbers or "Odd" for odd numbers.
+
+
+// Mål: 
+skapa en funktion som tar heltal som argument och returnerar "Jämn" för jämna nummer
+och "udda" för ojämna nummer.
+
+// Input:
+ett argument för funktionen Number
+
+return: 
+skickar tillbaka jämn eller ojämn
+
+
+// Step-by-step:
+funktion med ett argument som tar emot ett nummer.
+
+om nummret är delbart med 2
+returnerna "jämt"
+
+om nummret inte är delbart med 2
+returnera "udda"

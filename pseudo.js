1cc6ba8d468aeca1c5220257016701e589f1355c
DennisCederqvist <denniz.cederquist@gmail.com>
2025-09-24 10:16:16 +0200

    updated the js file with some more pseudokod

--- pseudo.js
+++ pseudo.js
@@ -5,3 +5,30 @@
 75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
 0, in other cases */
 
+
+Goal: calculate the finalgrade based on certain criterias
+
+
+Inputs:
+Start
+    input exampoints and sava as exam
+    input number of project save as projects
+
+    if exam > 90 or "||" if projects > than 10
+    return 100
+
+    if exam > 70 and "&&" projects >=5
+    return 90 points
+
+    if exam > 50 and "&&" projects >= 2
+    return 75
+
+    else
+    return 0
+End
+
+Returns:
+between 0 and 100 points depending on the results.
+
+Step by step:
+
